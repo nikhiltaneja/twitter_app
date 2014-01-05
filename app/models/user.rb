79c69086b6af5b7a85d27eb032b18472e25116d6
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-04 22:59:20 -0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
